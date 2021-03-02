9c82b70cc08ee90bd4c9ad6389905067d13e892d
rambodsm <rambodsm@outlook.com>
2021-03-02 13:55:16 +0330

    Add Generic Repository

---
+++ FoodOrder.Infrastructure/Contracts/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using FoodOrder.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Infrastructure.Contracts
+{
+    public interface IRepository<TEntity> where TEntity : class, IEntity
+    {
+        DbSet<TEntity> Entities { get; }
+        IQueryable<TEntity> Table { get; }
+        IQueryable<TEntity> TableNoTracking { get; }
+
+        void Add(TEntity entity);
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+        void AddRange(IEnumerable<TEntity> entities);
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
+        void Delete(TEntity entity, CancellationToken cancellationToken);
+        void DeleteRange(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+        TEntity GetById(params object[] ids);
+        ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids);
+        void Update(TEntity entity, CancellationToken cancellationToken);
+        void UpdateRange(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+    }
+}
---
+++ FoodOrder.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,88 @@
+﻿using FoodOrder.Common.Utilities;
+using FoodOrder.Domain.Common;
+using FoodOrder.Infrastructure.Contracts;
+using FoodOrder.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Infrastructure.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
+    {
+        protected readonly ApplicationDbContext DbContext;
+        public DbSet<TEntity> Entities { get; }
+        public virtual IQueryable<TEntity> Table => Entities;
+        public virtual IQueryable<TEntity> TableNoTracking => Entities.AsNoTracking();
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+            Entities = DbContext.Set<TEntity>();
+        }
+
+        #region Async Method
+        public virtual ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids)
+        {
+            return Entities.FindAsync(ids, cancellationToken);
+        }
+
+        public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            await Entities.AddAsync(entity, cancellationToken).ConfigureAwait(false);
+        }
+
+        public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entities, nameof(entities));
+            await Entities.AddRangeAsync(entities, cancellationToken).ConfigureAwait(false);
+        }
+
+        public virtual void Update(TEntity entity, CancellationToken cancellationToken)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Update(entity);
+        }
+
+        public virtual void UpdateRange(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+        {
+            Assert.NotNull(entities, nameof(entities));
+            Entities.UpdateRange(entities);
+        }
+
+        public virtual void Delete(TEntity entity, CancellationToken cancellationToken)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Remove(entity);
+        }
+
+        public virtual void DeleteRange(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+        {
+            Assert.NotNull(entities, nameof(entities));
+            Entities.RemoveRange(entities);
+        }
+        #endregion
+
+        #region Sync Methods
+        public virtual TEntity GetById(params object[] ids)
+        {
+            return Entities.Find(ids);
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Add(entity);
+        }
+
+        public virtual void AddRange(IEnumerable<TEntity> entities)
+        {
+            Assert.NotNull(entities, nameof(entities));
+            Entities.AddRange(entities);
+        }
+        #endregion
+    }
+}
